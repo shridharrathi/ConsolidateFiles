09b4cffdbb171f084ab5f58df3d05795753b7208
Shridhar Rathi <shridharrathi@gmail.com>
2018-05-19 12:10:33 +1200

    First commit

---
+++ ConsolidateFiles/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace ConsolidateFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(240, 71);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select Source Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(32, 197);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(240, 71);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Select Destination Folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(145, 333);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(240, 71);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Start Consolidation";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(315, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(315, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1126, 502);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+    }
+}
+
---
+++ ConsolidateFiles/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsolidateFiles
+{
+    public partial class Form1 : Form
+    {
+        private string _sourceFolder;
+        private string _destinationFolder;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                label1.Text = folderBrowserDialog1.SelectedPath;
+                _sourceFolder = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog2.ShowDialog() == DialogResult.OK)
+            {
+                label2.Text = folderBrowserDialog2.SelectedPath;
+                _destinationFolder = folderBrowserDialog2.SelectedPath;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(@"C:\temp"))
+            {
+                Directory.CreateDirectory(@"C:\temp");
+            }
+            var listAllFiles = Directory.GetFiles(_sourceFolder, "*.*", SearchOption.AllDirectories);
+            var count = listAllFiles.Count();
+            var errorCount = 0;
+
+            foreach (var file in listAllFiles)
+            {
+                FileInfo info = new FileInfo(file);
+                if (info.Extension.Equals(".ini"))
+                {
+                    count -= 1;
+                    continue;
+                }
+                var destinationFile = $"{_destinationFolder}\\{info.Name}";
+
+                if (destinationFile.Equals(file))
+                {
+                    count -= 1;
+                    continue;
+                }
+
+                if (!File.Exists(destinationFile))
+                {
+                    File.Move(file, destinationFile);
+                }
+                else
+                {
+                    using (StreamWriter logFile = new StreamWriter(@"C:\temp\FilesConsolidationFailLog.txt", true))
+                    {
+                        logFile.WriteLine($"{DateTime.Now.ToString("dd-MMM-yyyy")},{file},{destinationFile}");
+                    }
+                    errorCount++;
+                }
+            }
+            if (errorCount > 0)
+            {
+                if (errorCount == count)
+                {
+                    MessageBox.Show("Failed to move any files. The log file destination is C:\\temp\\FilesConsolidationFailLog.txt");
+                }
+                else
+                {
+                    MessageBox.Show($"Successfully moved {count - errorCount}. Failed to move {errorCount}. The log file destination is C:\\temp\\FilesConsolidationFailLog.txt");
+                }
+            }
+            else
+            {
+                MessageBox.Show($"Successfully moved all the files. File Count : {count}");
+            }
+
+
+        }
+    }
+}
